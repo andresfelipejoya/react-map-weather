e7a61311d696f8c1d915ac6c658de9cd07b7b0a7
Andres Joya <andres@AndresJoya>
2025-08-21 10:19:35 -0500

    :bug: Fix webpack

---
+++ src/pages/home/error-page.tsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <h1 className="text-2xl font-bold">404 - Página no encontrada 😢</h1>
+        </div>
+    );
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '@/pages/home/home.page';
+import ErrorPage from '@/pages/home/error-page';
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home />
+        element: <Home />,
+        errorElement: <ErrorPage />,
     }
-]);
+], { basename: '/react-map-weather' });
